0e5acc16252415ba2e2921d427b54ee78d88588d
zzhdsb123 <49418222+zzhdsb123@users.noreply.github.com>
2019-10-08 21:56:00 -0400

    no warnings

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 from app import views
