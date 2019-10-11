76ff76685d5954cfc6fd27a1b9be089d0ad38fcb
zzhdsb123 <zzhdsb123@gmail.com>
2019-10-11 10:28:11 -0400

    !@#$%^&*(

--- app/model.py
+++ app/model.py
@@ -8,3 +8,8 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}')"
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(200), nullable=False)
