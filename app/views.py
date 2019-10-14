d3442cc8418b5ea1612fce62551c90e649cf8519
zzhdsb123 <zzhdsb123@gmail.com>
2019-10-14 00:17:44 -0400

    api responding message fixed

--- app/views.py
+++ app/views.py
@@ -5,11 +5,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import text_detection, model, db, app
 from datetime import timedelta
 
+# the following four image extensions are allowed
 app.config["allowed_img"] = ["png", "jpg", "jpeg", "fig"]
 app.secret_key = os.urandom(24)
 
 
 def allowed_img(filename):
+    # a function which determines whether a filename(extension) is allowed
+    # str(filename) -> bool
+    # If the file extension in 'png', 'jpg', 'jpeg' and 'gif', return True, otherwise return False
     if "." not in filename:
         return False
     ext = filename.rsplit(".", 1)[1]
@@ -19,13 +23,10 @@ def allowed_img(filename):
         return False
 
 
-# @app.before_request
-# def expire():
-#     pass
-
-
 @app.route('/', methods=["GET", "POST"])
 def index():
+    # main page
+    # If the user has not logged out, redirect to the user page.
     if 'user' in session:
         return redirect(url_for('user'))
     return render_template('index2.html')
@@ -33,6 +34,9 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # registration page
+    # On the server side, check whether the username and password are valid or not.
+    # username and password must be strings between 2 to 100 characters
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
@@ -46,36 +50,37 @@ def register():
 
         flag = False
         if not 2 <= len(username) <= 100:
-            # flash("Invalid Username!")
             context['username_valid'] = 1
             flag = True
 
         if password != confirm_password:
-            # flash("Passwords do not match!")
             context['password_valid'] = 1
             flag = True
 
         if not 2 <= len(password) <= 100:
-            # flash("Password too long or too short!")
             context['pawconfirm_valid'] = 1
             flag = True
 
-        # avoid users with the same username
+        # users are not allowed to have same username
         dup_user = model.User.query.filter_by(username=username).first()
         if dup_user is not None:
-            # flash('Username already exists! Please choose another one!')
             context['username_valid'] = 2
             flag = True
         if flag:
             return render_template('signup.html', **context)
 
+        # Different users are allowed to have the same password
+        # After using salt value for storing passwords, they will look completely different on the server(database)
+        # even though they are the same
         password = generate_password_hash(password + username)
         candidate_user = model.User(username=username, password=password)
         db.session.add(candidate_user)
         db.session.commit()
+        # two directories are created to store the images later uploaded by the user
         os.system('cd app/static/users && mkdir ' + username)
         os.system('cd app/static/users/' + username + ' && mkdir ' + 'original')
         os.system('cd app/static/users/' + username + ' && mkdir ' + 'processed')
+        # log in
         session['user'] = username
         return redirect(url_for('user'))
     context = {
@@ -88,6 +93,8 @@ def register():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    # login page
+    # verify the username and password provided by the user
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -96,15 +103,16 @@ def login():
             candidate_user.username
         except:
             flash('Invalid username or password')
-            return render_template('login.html',p=1)
+            return render_template('login.html', p=1)
         if check_password_hash(candidate_user.password, password + username):
             session['user'] = username
             session.permanent = True
+            # after 24 hours, users are required to reenter their usernames and passwords for security purposes
             app.permanent_session_lifetime = timedelta(minutes=1440)
             return redirect(url_for('user', username=username))
         else:
             flash('Invalid username or password')
-            return render_template('login.html',p=1)
+            return render_template('login.html', p=1)
     else:
         return render_template('login.html')
 
@@ -117,6 +125,7 @@ def logout():
 
 @app.route('/user', methods=["GET", "POST"])
 def user():
+    # if the user are not logged in, redirect to the login page
     if 'user' not in session:
         flash('You are not logged in!')
         return redirect(url_for('index'))
@@ -126,11 +135,12 @@ def user():
 
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
+    # if the user are not logged in, redirect to the login page
     if 'user' not in session:
         flash('You are not logged in!')
         return redirect(url_for('index'))
     username = session['user']
-    
+    # verify the extension of the image which users want to upload
     if request.method == "POST":
         file = request.files['file']
         if request.files:
@@ -142,6 +152,7 @@ def upload():
                 return redirect(request.url)
 
             else:
+                # use a unique id to mark each image so that images with same name will not overwrite each other
                 filename = secure_filename(file.filename)
                 candidate_file = model.Image(filename=filename)
                 db.session.add(candidate_file)
@@ -152,16 +163,16 @@ def upload():
                 new_img_name = 'app/static/users/' + username + '/processed/' + name + '.' + ext
                 file.save(os.path.join("app/static/users/" + username + '/original/', name + '.' + ext))
                 east_location = "app/frozen_east_text_detection.pb"
+                # run the text detector and store the new image in the corresponding directory
                 text_detection.process_image(original_name, east_location, new_img_name)
                 flash("upload success!")
         return render_template('upload_success.html')
     return render_template('upload2.html')
 
 
-# display all the images in the folder
 @app.route("/preview")
 def preview():
-    # TODO: Split pages if number of imgs is huge
+    # display all the images in the users folder so that each user can only see the images he or she uploaded
     if 'user' not in session:
         flash('You are not logged in!')
         return redirect(url_for('index'))
@@ -171,16 +182,18 @@ def preview():
         cur_id = img_id.rsplit(".", 1)[0]
         try:
             cur_id = int(cur_id)
+            # display the original name of the photo
             img_name = model.Image.query.filter_by(id=cur_id).first().filename
         except:
             img_name = None
         hists[img_id] = img_name
-        # print(hists[img_id])
     return render_template('preview2.html', hists=hists, username=session['user'])
 
 
 @app.route('/fullImg/<img_id>')
 def fullImg(img_id):
+    # This function allows user to compared two images. One is the photo before text detection while the other is the
+    # one after text detection.
     if 'user' not in session:
         flash('You are not logged in!')
         return redirect(url_for('index'))
@@ -192,12 +205,12 @@ def api_register():
     username = request.form.get('username')
     password = request.form.get('password')
     if not isinstance(username, str) or not 2 <= len(username) <= 100:
-        return jsonify("invalid username!"), 406
+        return "406, invalid username!"
     if not isinstance(username, str) or not 2 <= len(password) <= 100:
-        return jsonify("invalid password!"), 406
+        return "406, invalid password!"
     dup_user = model.User.query.filter_by(username=username).first()
     if dup_user is not None:
-        return jsonify("username already exists!"), 406
+        return "406, username already exists!"
     password = generate_password_hash(password + username)
     candidate_user = model.User(username=username, password=password)
     db.session.add(candidate_user)
@@ -205,7 +218,7 @@ def api_register():
     os.system('cd app/static/users && mkdir ' + username)
     os.system('cd app/static/users/' + username + ' && mkdir ' + 'original')
     os.system('cd app/static/users/' + username + ' && mkdir ' + 'processed')
-    return jsonify("user created!"), 201
+    return "201, user created!"
 
 
 @app.route('/api/upload', methods=["POST", "GET"])
@@ -214,27 +227,30 @@ def api_upload():
     password = request.form.get('password')
     file = request.files['file']
     if not isinstance(username, str) or not 2 <= len(username) <= 100:
-        return jsonify("invalid username or password!"), 406
+        return "406, invalid username or password!"
     if not isinstance(username, str) or not 2 <= len(password) <= 100:
-        return jsonify("invalid username or password!"), 406
+        return "406, invalid username or password!"
     candidate_user = model.User.query.filter_by(username=username).first()
     if candidate_user is None:
-        return jsonify("invalid username or password!"), 406
+        return "406, invalid username or password!"
     if not check_password_hash(candidate_user.password, password + username):
-        return jsonify("invalid username or password!"), 406
+        return "406, invalid username or password!"
     if file.filename == "":
-        return jsonify("Image must have a filename"), 406
+        return "406, Image must have a filename"
     if not allowed_img(file.filename):
-        return jsonify("That image extension is not allowed!"), 406
+        return "406, That image extension is not allowed!"
     else:
         filename = secure_filename(file.filename)
+        candidate_file = model.Image(filename=filename)
+        db.session.add(candidate_file)
+        db.session.commit()
         name, ext = filename.rsplit(".", 1)
-        same_name = os.popen('cd app/static/users/' + username + '/original && find . -name ' + filename).read()
-        if same_name != "":
-            name += '(1)'
+        name = str(candidate_file.id)
         original_name = 'app/static/users/' + username + '/original/' + name + '.' + ext
         new_img_name = 'app/static/users/' + username + '/processed/' + name + '.' + ext
         file.save(os.path.join("app/static/users/" + username + '/original/', name + '.' + ext))
         east_location = "app/frozen_east_text_detection.pb"
+        # run the text detector and store the new image in the corresponding directory
         text_detection.process_image(original_name, east_location, new_img_name)
-        return jsonify("upload success!"), 201
+        return "201, upload success!"
+
