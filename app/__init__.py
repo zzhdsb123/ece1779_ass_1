e5563d39f1f0fd569a3c37147b98504ba982e81b
zzhdsb123 <zzhdsb123@gmail.com>
2019-10-15 16:51:13 -0400

    messages fixed

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ username='root'
 password='F=mdv/dt123'
 host='127.0.0.1'
 port='3306'
-database='ece1779'
+database='ece1779a1'
 
 SQLALCHEMY_DATABASE_URI="{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(dia,dri,username,password,host,port,database)
 
